9444e9d050c02dc108659dc3db34937cbce972b1
RyanAlves26 <ryan.alves@gestaokaizen.com.br>
2023-09-21 16:38:57 -0300

    Feito a leitura do arquivo html

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,21 +1,23 @@
-import Handlebars from 'handlebars';
+import fs from 'fs';
+import Handlebars, { template } from 'handlebars';
 
 interface ITemplateVariables {
   [key: string]: string | number;
 }
 
 interface IParseMailTemplate {
-  template: string;
+  file: string;
   variables: ITemplateVariables;
 }
 
 export default class HandlebarsMailTemplate {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = Handlebars.compile(template)
+  public async parse({ file, variables }: IParseMailTemplate): Promise<string> {
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf8',
+    });
 
-    return parseTemplate(variables)
+    const parseTemplate = Handlebars.compile(templateFileContent);
+
+    return parseTemplate(variables);
   }
 }
