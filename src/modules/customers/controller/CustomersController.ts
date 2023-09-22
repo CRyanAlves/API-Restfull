d3fa16e89b097e59c7d1fb120c3fe89665961c1d
RyanAlves26 <ryan.alves@gestaokaizen.com.br>
2023-09-22 15:16:51 -0300

    Feat-Customers-Controller

---
+++ src/modules/customers/controller/CustomersController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import ListCustomerService from '../service/ListCustomerService';
+import ShowCustomerService from '../service/ShowCustomerService';
+import CreateCustomerService from '../service/CreateCustomerService';
+import UpdateCustomerService from '../service/UpdateCustomerService';
+import DeleteCustomerService from '../service/DeleteCustomerService';
+
+export default class customersController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    const customers = await listCustomers.execute();
+
+    return res.json(customers);
+  }
+
+  public async find(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const findCustomer = new ShowCustomerService();
+
+    const customer = await findCustomer.execute({ id });
+
+    return res.json(customer);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email } = req.body;
+
+    const createCustomer = new CreateCustomerService();
+
+    const customer = await createCustomer.execute({ name, email });
+
+    return res.json(customer);
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const { name, email } = req.body;
+    const { id } = req.params;
+
+    const updateCustomer = new UpdateCustomerService();
+
+    const product = await updateCustomer.execute({ id, name, email });
+
+    return res.json(product);
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.execute({ id });
+
+    return res.json([]);
+  }
+}
